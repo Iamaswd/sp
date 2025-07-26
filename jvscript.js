3b0faf00791a686f956a69f794c6d079f8a644f7
Iamaswd <ducdung.ntc3@gmail.com>
2025-07-26 18:28:27 +0700

    wtff

--- jvscript.js
+++ jvscript.js
@@ -1,27 +1,28 @@
 let arr = [
     {
-        "name": "Cleansing Gel",
+        "name": "Toner Klairs",
         "price": 100,
-        "image": "./iMAGE/main.jpg"
+        "image": "https://klairsvietnam.vn/wp-content/uploads/2020/07/nuoc-hoa-hong-khong-mui-klairs.jpg"
     },
     {
-        "name": "Toner",
+        "name": "Toner Kombucha",
         "price": 150,
         "image": "./iMAGE/Copy of 15.jpg"
     },
     {
-        "name": "Moisturizer",
+        "name": "Toner Mamonde",
         "price": 200,
-        "image": "./iMAGE/main.jpg"
+        "image": "https://upload.wikimedia.org/wikipedia/commons/a/a7/Mamonde_Rose_Water_Toner.jpg"
     }
-]
+    
+];
+
 let getToner = document.getElementById("toner");
+
 getToner.innerHTML = arr.map((item) => {
-    return `<div class="product">
-                <img class="image-toner" src="${item.image}" alt="">
-                    <h2>${item.name}</h2>
-                    <p>Price: $${item.price}</p>
-                
+    return `<div>
+                <img class="image-toner" src="${item.image}" alt="${item.name}" style="width:300px; height:auto; border-radius:8px; margin-bottom:10px;">
+                <h2 style="font-size:23px; color:#333; margin-bottom:5px;">${item.name}</h2>
+                <p style="color:#333; font-size:16px;">Price: $${item.price}</p>
             </div>`;
 }).join("");
-console.log(getText);
